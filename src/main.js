da7ac610278a6dcfc3492c1d4ca82f5d8f1c6220
Viktorr22 <progdub1@gmail.com>
2023-01-21 12:42:33 +0200

    add markup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
-import { getData } from './api';
-import { createProductsMarkup } from './markup';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css';
+import { getData } from "./api";
+import { createProductsMarkup } from "./markup";
+import { listRef, formRef } from "./refs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+
+formRef.addEventListener("submit", onSubmit);
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  const searchValue = evt.target.qwery.value.trim();
+  if (!searchValue) {
+    return;
+  }
+  const { products } = await getData(`products/search?q=${searchValue}`);
+  console.log(products);
+  const markup = createProductsMarkup(products);
+  clearMarkup();
+  addListMarkup(markup);
+}
 
 async function init(e) {
-  const { products } = await getData('products');
+  const { products } = await getData("products");
   console.log(products);
   const markup = createProductsMarkup(products);
   console.log(markup);
+  addListMarkup(markup);
 }
 init();
+
+function addListMarkup(markup = "") {
+  listRef.insertAdjacentHTML("beforeend", markup);
+}
+
+function clearMarkup() {
+  listRef.innerHTML = "";
+}
+
+// const a = 5;
+// const c = 10;
+// const result = a + c;
+// // ^?
---
+++ src/refs/index.js
@@ -0,0 +1,2 @@
+export const listRef = document.querySelector(".js-row");
+export const formRef = document.querySelector(".d-flex");
