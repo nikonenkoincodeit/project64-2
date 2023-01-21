8d21ee044a1825bf9b1b327e0212c59ecc3780fd
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-21 11:06:20 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
+import { getData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+async function init(e) {
+  const product = await getData("products");
+  console.log(product);
+}
+init();
